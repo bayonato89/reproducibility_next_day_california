c0525b1774641f1e0e2fe3091db559347958dd6b
Toño Bayona <72937672+bayonato89@users.noreply.github.com>
2026-01-23 15:05:25 +0000

    Delete run_all.sh

--- run_all.sh
+++
@@ -1,6 +0,0 @@
-echo ''
-echo 'Running calculations'
-echo '============================'
-python code/reproducibility_next_day_california.py
-echo '============================'
-echo 'The reproducibility software package has run successfully.'
