dfe235122ab410d8722a83341a0e136a91fc79c2
Toño Bayona <72937672+bayonato89@users.noreply.github.com>
2026-01-23 12:48:34 +0000

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,6 @@
 echo ''
 echo 'Running calculations'
 echo '============================'
-
 python code/reproducibility_next_day_california.py
+echo '============================'
+echo 'The reproducibility software package has run successfully.'
